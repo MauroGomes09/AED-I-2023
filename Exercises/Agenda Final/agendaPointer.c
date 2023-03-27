117c11a9d186f0c1e081cfc79cd406341f9fa35b
MauroGomes09 <mauroarturmauro@gmail.com>
2023-03-27 00:13:22 -0300

    update

--- Exercises/Agenda Final/agendaPointer.c
+++ Exercises/Agenda Final/agendaPointer.c
@@ -20,7 +20,7 @@ Continuar a sua implementação da agenda (exercício 3) semana 1 dentro dos mes
 
 int *pBuffer = NULL;
 
-int *frenteRecebe ( int *pBuffer, void *pawn ) {//pawn aponta pro q estava sendo apontado pelo buffer, buffer aponta pra pawn
+int *frenteRecebe ( int *pBuffer, void *pawn ) { //pawn aponta para o que estava sendo apontado pelo buffer, buffer aponta para pawn
 	if ( !pBuffer )
 		return pawn;
 
@@ -29,8 +29,8 @@ int *frenteRecebe ( int *pBuffer, void *pawn ) {//pawn aponta pro q estava sendo
 	void **bufferBloco = (void*)&pBuffer;
 	pawn+=10*sizeof(char)+3*sizeof(int);
 
-	*(int *)pawn = (int)pBuffer;//pessoa q ta sendo adicionada aponta pro que o buffer apontava
-	*(int*)(*bufferBloco+10*sizeof(char)+2*sizeof(int)) = (int)iniPerson;//o prev da pessoa q tava no buffer aponta pra q entrou
+	*(int *)pawn = (int)pBuffer; //pessoa que está sendo adicionada aponta para o que o buffer apontava
+	*(int*)(*bufferBloco+10*sizeof(char)+2*sizeof(int)) = (int)iniPerson; //o prev da pessoa que estava no buffer aponta para o que entrou
 
 	return iniPerson;
 }
@@ -78,7 +78,7 @@ int *Push (int *pBuffer, void *pawn){
 		return pBuffer;
 	}
 
-	//compara se o elemento deve vir como primeiro
+	// compara se o elemento deve vir como primeiro
 	if (strcmp((char*)pBuffer,(char*)pawn)>=1){
 		pBuffer = frenteRecebe(pBuffer,pawn);
 		return pBuffer;
@@ -91,7 +91,7 @@ int *Push (int *pBuffer, void *pawn){
 	while ( 100 ){
 		prev = pBuffer;
 		if (strcmp((char*)pBuffer,(char*)pawn)>=1){
-			prev = (int*)*(int*)(*prevPessoa+10*sizeof(char)+2*sizeof(int));//prev recebe a pessoa de tras
+			prev = (int*)*(int*)(*prevPessoa+10*sizeof(char)+2*sizeof(int)); //prev recebe a pessoa de tras
 			*(int*)(*prevPessoa+10*sizeof(char)+3*sizeof(int)) = (int) pawn;
 			//nó da frente da pessoa de tras = nova pessoa
 	
@@ -109,16 +109,16 @@ int *Push (int *pBuffer, void *pawn){
 
 		pBuffer = *(int**)(*bufferBloco+10*sizeof(char)+3*sizeof(int)); //incrementa
 
-		if (!pBuffer){ //se for a ultima pessoa
+		if (!pBuffer){ //caso seja a última pessoa
 			finalRecebe ( prev,pawn );
 			return start;
 		} 
-		//só cai aqui se for no meio, então se a pessoa q eu to tentando adicionar vier antes da prox ele adiciona
+		//esse caso só acontece se for no meio, então se a pessoa que estou tentando adicionar vier antes da próxima, então adiciona-se
 	}
 	return 0;
 }
 
-int *Pop ( int *pBuffer ) { //apaga o primeiro elemento pois segue as regras da fila
+int *Pop ( int *pBuffer ) { //apaga o primeiro elemento para seguir as regras da fila
 	void **bufferBloco = (void*)&pBuffer;
 	void *exc = pBuffer;
 
@@ -131,7 +131,7 @@ int *Pop ( int *pBuffer ) { //apaga o primeiro elemento pois segue as regras da 
 
 	if ( *(int*)(*bufferBloco+10*sizeof(char)+2*sizeof(int))==0 ) {
 		pBuffer = (int*)*(int*)(*bufferBloco+SIZE-sizeof(int));
-		//pBuffer vai pra prox pessoa
+		//pBuffer vai para a próxima pessoa
 		*(int*)(*bufferBloco+10*sizeof(char)+2*sizeof(int)) = 0;
 		//limpa o prev da segunda pessoa
 		free ( exc );
@@ -140,10 +140,10 @@ int *Pop ( int *pBuffer ) { //apaga o primeiro elemento pois segue as regras da 
 	return 0;
 }
 
-void finalRecebe(int *pBuffer, void *pawn){ //faz o next da ultima receber pawn, e o prev de pawn receber essa ultima
+void finalRecebe(int *pBuffer, void *pawn){ //faz o next da última receber pawn, e o prev de pawn receber essa última
 	void **bufferBloco = (void*)&pBuffer;
 	int *nextLastPerson = (int*)(*bufferBloco+10*sizeof(char)+3*sizeof(int));
-	//.next da ultima pessoa do buffer
+	//.next da última pessoa do buffer
 
 	*nextLastPerson = (int)pawn;
 	pawn += SIZE-2*sizeof(int); //vai pro prev de pawn
@@ -199,7 +199,7 @@ int main() {
 						free ( exc );
 						free ( pawn );
 					}	
-					printf ( "\nBuffer Limpo e saindo do menu!" );
+					printf ( "\nSaindo do menu e limpando buffer!" );
 				}
 				exit ( 0 );
 				break;
