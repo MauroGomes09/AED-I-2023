ff5121d6ffbe6171e2ee4ddc41598e7fd51b7246
MauroGomes09 <mauroarturmauro@gmail.com>
2023-03-27 00:06:13 -0300

    update

--- Exercises/Agenda Final/agendaPointer.c
+++ Exercises/Agenda Final/agendaPointer.c
@@ -1,6 +1,15 @@
 /* 
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 Nome: Mauro Artur Gomes de Oliveira - N de matrícula: 21102813 - AGENDA - Estrutura de código utilizada: Doom 3 Code Style Conventions
+
+Continuar a sua implementação da agenda (exercício 3) semana 1 dentro dos mesmo parâmetros, mas incluir o seguinte.
+--Nenhuma variável pode ser declarada em todo o programa, somente ponteiros. Todos os dados do programa devem ser guardados dentro do pBuffer.
+--Nem mesmo como parâmetro de função. Só ponteiros que apontam para dentro do pBuffer.
+--Exemplo do que não pode: int c; char a; int v[10];  void Funcao(int parametro)
+--Não pode usar struct em todo o programa.
+--Usar fila ordenada (heap) para armazenar as pessoas em ordem alfabética sempre que o usuário incluir uma nova pessoa.
+--Implementar a base de dados da agenda usando lista duplamente ligada
+--Somente essa base de dados pode ficar fora do buffer principal, ou seja, pode usar um malloc para cada nodo.
 ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 */ 
 
